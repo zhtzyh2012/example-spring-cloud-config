de1325d6758df94dc9e02c496cf9ade584e12958
zhanghongtu <zhanghongtu@quandashi.com>
2017-12-12 18:20:01 +0800

    处理config-server的module入口类和yml配置文件

--- config-server/src/main/java/com/qds/optimize/config/server/ServerApplication.java
+++ config-server/src/main/java/com/qds/optimize/config/server/ServerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  *
  * @author neutron
  */
-//@EnableConfigServer
+@EnableConfigServer
 @EnableEurekaClient
 @SpringBootApplication
 public class ServerApplication {
